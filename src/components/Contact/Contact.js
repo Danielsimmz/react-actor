48863072c631d1fad6360490df212d236dc90948
Daniel Simango <daniel.simmz@gmail.com>
2020-06-09 12:16:38 -0500

    done base goals

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <header>
+                <h2>Contact</h2>
+                <p>Phone: +13628137139</p>
+                <p>Email: BillyJean@gmail.com</p>
+            </header>
+            
+        );
+    }
+}
+export default Contact;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <b><i>© React Actors</i></b>
+            </footer>
+            
+        );
+    }
+}
+export default Footer;
